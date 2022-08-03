1ad0088ca8eaef5748bcbb0c6db204297be759e1
jbped <jake.pedigo@lendio.com>
2022-08-03 11:35:15 -0600

    added image, name, role, and contact info

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        // appBar: AppBar(
+        //   backgroundColor: Colors.tealAccent,
+        //   title: Text(
+        //     'Hello',
+        //     style: TextStyle(color: Colors.black),
+        //   ),
+        // ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/profile_pic.jpeg'),
+              ),
+              Text(
+                'Jake Pedigo',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26.0),
+              ),
+              Text(
+                'Junior Software Engineer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Source Sans',
+                  fontSize: 18.0,
+                  letterSpacing: 3.0,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 180,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: ListTile(
+                  dense: true,
+                  iconColor: Colors.teal,
+                  leading: Icon(Icons.phone),
+                  title: Text('###-###-####'),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: ListTile(
+                  dense: true,
+                  iconColor: Colors.teal,
+                  leading: Icon(Icons.email),
+                  title: Text('email@gmail.com'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
